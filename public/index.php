81b0d1611abbff115e092692c4f15ee5028afa26
Kirsch Julio <kirschjulio@gmail.com>
2022-10-03 14:41:16 +0200

    design

--- public/index.php
+++ public/index.php
@@ -2,19 +2,39 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../resources/css/app.css" type="text/css">
+<link rel="stylesheet" href="../resources/css/app.css" type="text/css">
     <title></title>
   </head>
 
-  <div class="actions">
+<style media="screen">
+div.annonce{
+ padding: 15px;
+ background: #6495ED;
+ background-clip: border-box;
+}
 
-  <input type="text" name="" value="">
-  </div>
+h1 {
+  color: white;
+}
+</style>
+
   <body>
 
+ <form class="" action="index.html" method="post">
+
+ </form>
+ <div class="annonce">
+
+
+ <h1>SICHT AN 9 165 DOUDESANNONCEN</h1>
   <?php
-
+  echo "
+  <form class='' action='index.php' method='post'>
+   <input type='text' name='' value='' placeholder='Virnumm'> <input type='text' name='' value='' placeholder='Familljennumm'>
+   <button type='submit'><i class='fa fa-search'></i></button> <input type='date' name='' value='' placeholder='14.09.2022'>
+  </form>
+  ";
   ?>
-
+ </div>
   </body>
 </html>
