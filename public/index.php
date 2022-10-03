cd327638ef79062ef596dc22246e4d8822cfa1fe
Kirsch Julio <kirschjulio@gmail.com>
2022-10-03 10:20:31 +0200

    memento.lu

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../resources/css/app.css" type="text/css">
+    <title></title>
+  </head>
+
+  <div class="actions">
+
+  <input type="text" name="" value="">
+  </div>
+  <body>
+
+  <?php
+
+  ?>
+
+  </body>
+</html>
