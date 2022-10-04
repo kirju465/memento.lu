34ea79de30096f19fdd24abd46bc02d3982399f4
Kirsch Julio <kirschjulio@gmail.com>
2022-10-04 09:37:30 +0200

    search button

--- public/index.php
+++ public/index.php
@@ -44,8 +44,20 @@ button[type="submit"] {
   cursor: pointer;
   opacity: 0.7;
 }
-</style>
 
+form.example button {
+  float: left;
+  width: 20%;
+  padding: 10px;
+  background: #2196F3;
+  color: white;
+  font-size: 17px;
+  border: 1px solid grey;
+  border-left: none; 
+  cursor: pointer;
+}
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <body>
 
  <form class="" action="index.html" method="post">
